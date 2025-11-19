42b1224b9dc7b224deab3bb14a579dde44614496
contentnjbot <abooad2211@gmail.com>
2025-11-19 22:19:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot running!"
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)

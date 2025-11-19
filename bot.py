46b237068dbab97f03cc3d58df9fefadf7ae6434
contentnjbot <abooad2211@gmail.com>
2025-11-19 22:32:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,12 @@
 import os
-from flask import Flask
 import telebot
 
-TOKEN = os.environ.get("BOT_TOKEN")  # مهم: سنضيفه كـ secret لاحقًا
+TOKEN = os.environ["BOT_TOKEN"]
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-# نقطة استقبال أوامر تيليجرام
-@app.route("/" + TOKEN, methods=["POST"])
-def webhook():
-    json_updates = flask.request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_updates)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-# رسالة /start
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "تم تشغيل البوت بنجاح 🤖🔥")
+def start(msg):
+    bot.reply_to(msg, "Bot is running!")
 
-# لتشغيل السيرفر
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    bot.infinity_polling()
